3c4c8be8ac6211c00dda7d259f8ba8bdbc825c57
Shana <devfusediboot@protonmail.com>
2020-10-08 18:16:50 -0700

    Commented the program better

--- spacecalc.c
+++ spacecalc.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// This program takes a number in GB and turns it into KB, MB, and B
+/* A program I wrote for practice which takes a number I input in Gigabytes and turns it into Megabytes,
+ * Kilobytes, and Bytes.
+ * Example usage: $ ./spacecalc 5
+ *                   5368709120 B
+ *                   5242880 KB
+ *                   5120 MB
+ */
+
+//TODO: Add a check so it doesn't segmentation fault with no argument
+
 // main function
-int main(int argc, char *argv[]) {
-    long long a;
+int main(int argc, char *argv[]) { // takes a number as input
+    long long a; // defining each integer as long long int
     long long gb;
     long long mb;
     long long kb;
@@ -15,15 +24,15 @@ int main(int argc, char *argv[]) {
     
     // B calculation
     b = a * 1024 * 1024 * 1024; // takes a number and multiplies it three times by 1024
-    printf("%lld B\n",b);
+    printf("%lld B\n",b); // print it
     
     // KB calculation
     kb = a * 1024 * 1024; // takes a number and multiplies it twice by 1024
-    printf("%lld KB\n",kb);
+    printf("%lld KB\n",kb); // print it
 
     // MB calculation
     mb = a * 1024; // takes a and multiplies it by 1024
-    printf("%lld MB\n",mb);
+    printf("%lld MB\n",mb); // print it
     
     return 0;
 }
